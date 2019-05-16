4d48abb7bae1c0625ec5259de5b0ee5dcfde3834
Angela Lo <angelawenlo@gmail.com>
2019-05-16 13:49:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 require 'pry'
 def reverse_each_word(sentence)
 convert = sentence.split( )
+binding.pry
 new_array = []
 convert.each do |reversed|
   new_array << reversed
   new_array.reverse
-  binding.pry
+
   end
 end
