bf20ecd958b90431bb7a06abcb839cddbdcd2d2c
Angela Lo <angelawenlo@gmail.com>
2019-05-16 14:06:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,6 @@ def reverse_each_word(sentence)
 convert = sentence.split( )
 new_array = []
 convert.collect do |reversed|
-  binding.pry
-  reversed.reverse
+  new_array << reversed.reverse
   end
 end
